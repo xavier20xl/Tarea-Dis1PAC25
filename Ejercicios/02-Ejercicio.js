f0888ad6399acb73ba5a254141a4641ce6e9cbb8
Carlos Xavier López Mendoza <clopez@FUNADEH.ORG>
2025-02-04 10:58:40 -0600

    Update 02-Ejercicio.js #2

--- Ejercicios/02-Ejercicio.js
+++ Ejercicios/02-Ejercicio.js
@@ -12,7 +12,7 @@ function actualizarStock(productos, id, nuevoStock) {
     return null;
 }
 
-// Ejemplo de uso
+
 const productos = [ 
   { id: 1, nombre: "Lápiz", stock: 10 }, 
   { id: 2, nombre: "Cuaderno", stock: 5 }, 
